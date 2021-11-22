5baa26bedcfcc23a572586902e782e8ed5c76a79
fareen341 <59610617+fareen341@users.noreply.github.com>
2021-11-22 11:37:30 +0530

    Update settings.py

--- FoodOdering/settings.py
+++ FoodOdering/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!07msnpuwn@3y+^nl6_izesaap4^ior)i18ndf^y65tg3rziek'
+SECRET_KEY = ****
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
